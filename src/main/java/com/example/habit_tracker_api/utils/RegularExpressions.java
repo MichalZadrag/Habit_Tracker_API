866fc93561d8ce6cbf380ba5a155d55208c8759b
MichalZadrag <michal.zadrag98@gmail.com>
2021-01-04 21:29:34 +0100

    Poprawa wyrazenia regularnego

--- src/main/java/com/example/habit_tracker_api/utils/RegularExpressions.java
+++ src/main/java/com/example/habit_tracker_api/utils/RegularExpressions.java
@@ -2,7 +2,7 @@ package com.example.habit_tracker_api.utils;
 
 public final class RegularExpressions {
 
-    public static final String HABIT_AND_TASK_AND_EVENT_REGEXP = "^[a-zA-ZąćęłńóśźżĄĘŁŃÓŚŹŻ]{3,15}?( +[a-zA-ZąćęłńóśźżĄĘŁŃÓŚŹŻ]{2,6})?( +[a-zA-ZąćęłńóśźżĄĘŁŃÓŚŹŻ]{2,7})?$";
+    public static final String HABIT_AND_TASK_AND_EVENT_REGEXP = "^[a-zA-ZąćęłńóśźżĄĘŁŃÓŚŹŻ]{3,15}?( +[a-zA-ZąćęłńóśźżĄĘŁŃÓŚŹŻ]{2,9})?( +[a-zA-ZąćęłńóśźżĄĘŁŃÓŚŹŻ]{2,9})?$";
 
     public static final String LOCATION_REGEXP = "^[a-zA-Z0-9ąćęłńóśźżĄĘŁŃÓŚŹŻ]{3,10}$";
 
