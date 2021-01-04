26f12bca35ff5a54799239c2f0206999d74d7952
MichalZadrag <michal.zadrag98@gmail.com>
2021-01-04 15:20:39 +0100

    Test cronu

--- src/main/java/com/example/habit_tracker_api/controller/HabitController.java
+++ src/main/java/com/example/habit_tracker_api/controller/HabitController.java
@@ -109,7 +109,7 @@ public class HabitController {
         return ResponseEntity.ok(new ApiResponse(true, "Pomyślnie zmieniono"));
     }
 
-    @Scheduled(cron = "0 0 6 * * *")
+    @Scheduled(cron = "0 40 16 * * *")
     public void resetSeries() {
         List<Habit> habits = habitRepository.findAll();
         int oneDayInMs = 86400000;
